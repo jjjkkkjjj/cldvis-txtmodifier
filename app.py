88300d4fb73f295940d2e425eb6b4098a2581ba7
jjjkkkjjj <kado_89_ssp_jun@yahoo.co.jp>
2020-11-29 22:53:07 +0900

    for Mac Big sur

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from PySide2.QtWidgets import *
 import sys
 
+import os
+# to avoid not working in MAC Big sur
+os.environ['QT_MAC_WANTS_LAYER'] = '1'
+
 from tda.gui import MainWindowController
 
 if __name__ == '__main__':
